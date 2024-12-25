41f95824ec75c22da586445208d0bb862aab0c84
shiva8188 <chintubolla891@gmail.com>
2024-12-25 12:36:29 +0530

    Update styledComponent.js

--- src/components/Home/styledComponent.js
+++ src/components/Home/styledComponent.js
@@ -13,10 +13,10 @@ export const HomeContainer = styled.div`
   color: ${props => props.color};
 `
 export const BannerCon = styled.div`
-  position: fixed;
   background-image: url('https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png');
   background-size: cover;
   display: flex;
+  position: top;
   justify-content: space-between;
   padding: 25px;
 `
